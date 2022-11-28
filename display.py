0d3c96234f29531c031c2829d29f70feac8f18b3
TomBric <47101971+TomBric@users.noreply.github.com>
2022-11-28 08:21:03 +0100

    Rudder trim added and many improvements

--- display.py
+++ display.py
@@ -34,6 +34,8 @@ import epaper1in54
 from machine import SPI, Pin
 import framebuf
 import math
+import font8x8
+
 # Connection of the display
 #   Display   Board name   Board number
 # 	VCC	grey		3,3V		36
@@ -49,92 +51,188 @@ import math
 black = 0
 white = 1
 
+# Settings
+INDICATOR_LINE = 4
+INDICATOR_END = 16
+INDICATOR_NEUTRAL = 16
+SIZE_VOLT = 16
+SIZE_TRIANGLE = 30
+SIZE_TRIANGLE_POINTER = 12
+
 # seven segment numbers for display
-nums = ((1,3,4,5,6,7),(6,7), (1,6,2,5,3), (1,6,2,7,3), (4,6,2,7), (1,4,2,7,3), (1,4,2,7,5,3),
-		(1,6,7), (1,2,3,4,5,6,7), (1,4,6,2,7,3))
+nums = ((1, 3, 4, 5, 6, 7), (6, 7), (1, 6, 2, 5, 3), (1, 6, 2, 7, 3), (4, 6, 2, 7), (1, 4, 2, 7, 3), (1, 4, 2, 7, 5, 3),
+        (1, 6, 7), (1, 2, 3, 4, 5, 6, 7), (1, 4, 6, 2, 7, 3))
 
 
 # Default assignment: sck=Pin(10), mosi=Pin(11), miso=Pin(8)
 class Display:
-	def __init__(self):
-		spi = SPI(1, 32000000, polarity=0, phase=0, sck=Pin(10), mosi=Pin(11), miso=Pin(8))
-		cs = Pin(6)
-		dc = Pin(7)
-		rst = Pin(9)
-		busy = Pin(12)
+    def __init__(self, rudder_trim):
+        spi = SPI(1, 32000000, polarity=0, phase=0, sck=Pin(10), mosi=Pin(11), miso=Pin(8))
+        cs = Pin(6)
+        dc = Pin(7)
+        rst = Pin(9)
+        busy = Pin(12)
 
-		self.e = epaper1in54.EPD(spi, cs, dc, rst, busy)
-		self.e.init(False)
-		self.e.clear(0xFF)  # necessary to overwrite everything
-		self.e.init(True)
-		self.e.clear(0xFF)  # necessary to overwrite everything
-		self.buf = bytearray(self.e.width * self.e.height // 8)
-		self.fb = framebuf.FrameBuffer(self.buf, self.e.width, self.e.height, framebuf.MONO_HLSB)
-		self.fb.fill(white)
+        self.e = epaper1in54.EPD(spi, cs, dc, rst, busy)
+        self.e.init(False)
+        self.e.clear(0xFF)  # necessary to overwrite everything
+        self.e.init(True)
+        self.e.clear(0xFF)  # necessary to overwrite everything
+        self.buf = bytearray(self.e.width * self.e.height // 8)
+        self.fb = framebuf.FrameBuffer(self.buf, self.e.width, self.e.height, framebuf.MONO_HLSB)
+        self.fb.fill(white)
 
-	def print(self):
-		self.e.display_part(self.buf)
-		# self.fb.fill_rect(0, 0, self.e.width, self.e.height, white)
+        if rudder_trim:
+            self.indicator_hor = self.e.width - 10  # horizontal position of line on the right
+            self.indicator_up = 10  # start of indicator line
+            self.indicator_down = 150  # where indicator stops
+        else:
+            self.indicator_hor = self.e.width - 20  # horizontal position of line on the right
+            self.indicator_up = 10  # start of indicator line
+            self.indicator_down = self.e.height - 10  # where indicator stops
+        self.indicate_rudder = rudder_trim
 
-	def busy(self):
-		return self.e.busy()
+    def print(self):
+        self.e.display_part(self.buf)
 
-	def seven_seg_char(self, x, y, size, thick, character):
-		if character == '+':
-			self.fb.fill_rect(x, y + size//2 - thick//2, size//2, thick, black)   # "-" part
-			self.fb.fill_rect(x + size//4 - thick//2, y + size//4, thick, size//2, black)  # "|" part
-		elif character == '-':
-			self.fb.fill_rect(x, y + size // 2 - thick // 2, size // 2, thick, black)
-		elif character == '.':
-			self.fb.fill_rect(x + size//4 - thick//2, y + size - thick//2, thick, thick, black)
-		elif '0' <= character <= '9':
-			number = ord(character) - ord('0')
-			for led in nums[number]:
-				if led == 1:
-					self.fb.fill_rect(x, y, size//2+thick, thick, black)
-				elif led == 2:
-					self.fb.fill_rect(x, y+size//2, size//2+thick, thick, black)
-				elif led == 3:
-					self.fb.fill_rect(x, y+2*size//2, size//2+thick, thick, black)
-				elif led == 4:
-					self.fb.fill_rect(x, y, thick, size//2+thick, black)
-				elif led == 5:
-					self.fb.fill_rect(x, y+size//2, thick, size//2+thick, black)
-				elif led == 6:
-					self.fb.fill_rect(x+size//2, y, thick, size//2+thick, black)
-				elif led == 7:
-					self.fb.fill_rect(x+size//2, y+size//2, thick, size//2+thick, black)
+    # self.fb.fill_rect(0, 0 self.e.width, self.e.height, white)
 
-	def seven_seg_number(self, x, y, size, format_string, number):   # print an int value as seven segments
-		# example seven_seg_float(10,30, 10,'{:+2.1}', 12.7)
-		s = format_string.format(number)
-		xpos = x
-		for c in s:
-			self.seven_seg_char(xpos, y, size, size//8, c)
-			xpos += size//2 + size//4
+    def busy(self):
+        return self.e.busy()
 
-	def indicator(self, percentage, power, setupmode=0):
-		# self.fb.fill_rect(0, 0, self.e.width, self.e.height, white)
-		self.fb.fill(white)
-		s = 4  # size of out frame
-		sb = 16  # size of indicator
-		size_perc = 32  # size of numbers
-		size_volt = 16
+    def seven_seg_char(self, x, y, size, thick, character):
+        if character == '+':
+            self.fb.fill_rect(x, y + size // 2 - thick // 2, size // 2, thick, black)  # "-" part
+            self.fb.fill_rect(x + size // 4 - thick // 2, y + size // 4, thick, size // 2, black)  # "|" part
+        elif character == '-':
+            self.fb.fill_rect(x, y + size // 2 - thick // 2, size // 2, thick, black)
+        elif character == '.':
+            self.fb.fill_rect(x + size // 4 - thick // 2, y + size - thick // 2, thick, thick, black)
+        elif '0' <= character <= '9':
+            number = ord(character) - ord('0')
+            for led in nums[number]:
+                if led == 1:
+                    self.fb.fill_rect(x, y, size // 2 + thick, thick, black)
+                elif led == 2:
+                    self.fb.fill_rect(x, y + size // 2, size // 2 + thick, thick, black)
+                elif led == 3:
+                    self.fb.fill_rect(x, y + 2 * size // 2, size // 2 + thick, thick, black)
+                elif led == 4:
+                    self.fb.fill_rect(x, y, thick, size // 2 + thick, black)
+                elif led == 5:
+                    self.fb.fill_rect(x, y + size // 2, thick, size // 2 + thick, black)
+                elif led == 6:
+                    self.fb.fill_rect(x + size // 2, y, thick, size // 2 + thick, black)
+                elif led == 7:
+                    self.fb.fill_rect(x + size // 2, y + size // 2, thick, size // 2 + thick, black)
 
-		self.fb.fill_rect(100, 0, self.e.width-1-100, self.e.height-1, black)
-		self.fb.fill_rect(100+s, s, self.e.width-1-100-2*s, self.e.height-1-2*s, white)
-		self.fb.fill_rect(100+s, 100-s//2, self.e.width-1-100-2*s, s, black)
+    def seven_seg_number(self, x, y, size, format_string, number):  # print an int value as seven segments
+        # example seven_seg_float(10,30, 10,'{:+2.1}', 12.7)
+        s = format_string.format(number)
+        xpos = x
+        for c in s:
+            self.seven_seg_char(xpos, y, size, size // 8, c)
+            xpos += size // 2 + size // 4
 
-		if setupmode == 0 or setupmode == 1:
-			position = math.floor((self.e.height-2*s-sb) / 200 * percentage) + 100
-			self.fb.fill_rect(100+4*s, position-sb//2, 100-8*s, sb, black)
-			self.seven_seg_number(5, 100+s-size_perc//2, size_perc,'{:+3d}', percentage)
-			self.seven_seg_number(5, 5, size_volt, '{:+2.1f}', power)
-		if setupmode >= 1:    # indicate setup mode
-			self.fb.fill_rect(5, 0, 15, 15, black)   # black indication left upper corner
-			if setupmode == 2:
-				self.fb.fill_rect(30, self.e.height - sb, 100 - 8 * s, sb, black)
-			elif setupmode == 3:
-				self.fb.fill_rect(30, 100-sb//2, 100 - 8 * s, sb, black)
-			elif setupmode == 4:
-				self.fb.fill_rect(30, 0, 100 - 8 * s, sb, black)
+    def text(self, t, xpos, ypos, size):  # print a text at x y position, size should be a multiple of 8
+        pixelsize = size // 8
+        for i in range(0, len(t)):
+            if 'A' <= t[i] <= 'Z':
+                font = font8x8.font8x8_capitals
+                index = ord(t[i]) - ord('A')
+            elif 'a' <= t[i] <= 'z':
+                font = font8x8.font8x8_smalls
+                index = ord(t[i]) - ord('a')
+            elif '0' <= t[i] <= '9':
+                font = font8x8.font8x8_numbers
+                index = ord(t[i]) - ord('0')
+            elif '!' <= t[i] <= '&':
+                font = font8x8.font8x8_specials
+                index = ord(t[i]) - ord('!')
+            elif t[i] == '?':
+                font = font8x8.font8x8_specials
+                index = 1    # '?' is not in ascii iteration, so handle it special
+            else:
+                continue
+
+            for y in range(0, 8):
+                for x in range(0, 8):
+                    if font[index * 8 + y] & (128 >> x):
+                        self.fb.fill_rect(xpos + x * pixelsize, ypos + y * pixelsize, pixelsize, pixelsize, black)
+            xpos += 8 * pixelsize
+
+    def indicator(self, percentage, rudder_percentage, power, setupmode):
+
+        self.fb.fill(white)
+        # self.fb.fill_rect(0, 0, self.e.width, self.e.height, white)
+
+        self.text('Trim', 5, 5, 24)
+        self.text('Ind', 17, 32, 24)
+        self.seven_seg_number(13, 90, SIZE_VOLT, '{:+2.1f}', power)
+        self.text('V', 78, 92, 16)
+
+        if setupmode == 0 or setupmode == 1:
+            self.elevator_indicator(percentage)
+            if self.indicate_rudder:
+                self.rudder_indicator(rudder_percentage)
+            if setupmode == 1:  # indicate waiting for another button press
+                self.text('Setup?', 5, 120, 16)
+        if setupmode >= 2:  # indicate setup mode
+            # self.fb.fill_rect(5, 0, 15, 15, black)   # black indication left upper corner
+            self.text('Setup', 5, 120, 16)
+            if setupmode == 2:  # setup trim up
+                self.elevator_indicator(+100)
+            elif setupmode == 3:  # setup trim neutral
+                self.elevator_indicator(0)
+            elif setupmode == 4:  # setup trim down
+                self.elevator_indicator(-100)
+            elif setupmode == 5:  # setup rudder left
+                self.rudder_indicator(+100)
+            elif setupmode == 6:  # setup rudder neutral
+                self.rudder_indicator(0)
+            elif setupmode == 7:  # setup rudder right
+                self.rudder_indicator(-100)
+
+    def elevator_indicator(self, percentage):
+        zeroy = self.indicator_up + math.floor((self.indicator_down - self.indicator_up) / 2)
+        length_indicator = self.indicator_down - self.indicator_up
+        self.fb.fill_rect(self.indicator_hor - INDICATOR_LINE, self.indicator_up, INDICATOR_LINE,
+                          length_indicator, black)
+        self.fb.fill_rect(self.indicator_hor - INDICATOR_END, self.indicator_up - INDICATOR_LINE//2,
+                          INDICATOR_END, INDICATOR_LINE, black)  # upper end line
+        self.text('DN', self.indicator_hor - INDICATOR_END - 2 * 16 - 4, self.indicator_up, 16)
+        self.fb.fill_rect(self.indicator_hor - INDICATOR_END, self.indicator_down - INDICATOR_LINE//2,
+                          INDICATOR_END, INDICATOR_LINE, black)  # down end line
+        self.text('UP', self.indicator_hor - INDICATOR_END - 2 * 16 - 4, self.indicator_down - 12, 16)
+        self.fb.fill_rect(self.indicator_hor - INDICATOR_END, zeroy - INDICATOR_LINE // 2, INDICATOR_END,
+                          INDICATOR_LINE, black)  # neutral line
+
+        posy = zeroy + math.floor(length_indicator / 200 * percentage)
+
+        for i in range(0, SIZE_TRIANGLE // 4):
+            self.fb.fill_rect(self.indicator_hor - SIZE_TRIANGLE - SIZE_TRIANGLE_POINTER + i * 4,
+                              posy - SIZE_TRIANGLE // 2 + i * 2, 4, SIZE_TRIANGLE - i * 4, black)
+        self.fb.fill_rect(self.indicator_hor - SIZE_TRIANGLE_POINTER - INDICATOR_LINE, posy - INDICATOR_LINE // 2,
+                          SIZE_TRIANGLE_POINTER, INDICATOR_LINE, black)
+
+    def rudder_indicator(self, percentage):
+        self.text('L', 0, self.e.height - 16, 16)
+        self.text('R', self.e.width - 16, self.e.height - 16, 16)
+        self.fb.fill_rect(16, self.e.height - INDICATOR_LINE, self.e.width - 1 - 2 * 16,
+                          INDICATOR_LINE, black)  # long line
+        self.fb.fill_rect(16, self.e.height - 1 - INDICATOR_END, INDICATOR_LINE, INDICATOR_END, black)  # end line left
+        self.fb.fill_rect(self.e.width - 1 - 16 - INDICATOR_LINE, self.e.height - 1 - INDICATOR_END, INDICATOR_LINE,
+                          INDICATOR_END, black)
+        # end line right
+        self.fb.fill_rect(self.e.width // 2 - INDICATOR_LINE // 2, self.e.height - 1 - INDICATOR_NEUTRAL,
+                          INDICATOR_LINE, INDICATOR_NEUTRAL, black)
+        # neutral line
+
+        posx = 16 + INDICATOR_LINE + math.floor(((self.e.width - 32 - 2 * INDICATOR_LINE) // 2) +
+                                                percentage / 100 * ((self.e.width - 32 - 2 * INDICATOR_LINE) // 2))
+        for i in range(0, SIZE_TRIANGLE // 4):
+            self.fb.fill_rect(posx - SIZE_TRIANGLE // 2 + i * 2,
+                              self.e.height - 1 - SIZE_TRIANGLE_POINTER - SIZE_TRIANGLE + i * 4,
+                              SIZE_TRIANGLE - i * 4, 4, black)
+        self.fb.fill_rect(posx - INDICATOR_LINE // 2, self.e.height - 1 - INDICATOR_LINE - SIZE_TRIANGLE_POINTER,
+                          INDICATOR_LINE, SIZE_TRIANGLE_POINTER, black)
--- main.py
+++ main.py
@@ -46,67 +46,102 @@ DISPLAY_WAKEUP = const(50)      # number of refreshs after start, to get better 
 DIVIDER_R1 = 10000               # resistance in Ohms of R1 resistor connected to main power
 DIVIDER_R2 = 1000                # resistance in Ohms of R2 resistor of voltage divider
 VOLTAGE_FACTOR = 3.3 / 65536
+RUDDER_TRIM = True              # True if also rudder trim is desired
 
 # GLOBALS
 start = time.ticks_ms()
 user_status = 0
 trim_value = 0
+rudder_value = 0
 main_power = 0   # power voltage of aircraft
-trim_default = {'full_up': -100, 'neutral': 0, 'full_down': 100}
+trim_default = {'full_up': -100, 'neutral': 0, 'full_down': 100, 'rudder_left': -100, 'rudder_neutral': 0,
+                'rudder_right': 100}
 # settings for trim, initialized during setup (highest up, neutral, highest down)
 trim_settings = {}
-new_trim = {}
 display_wakeup = DISPLAY_WAKEUP
 led_onboard = Pin(25, Pin.OUT)
 
 
+def calc_display_percent(value):
+    diff = abs(trim_settings['neutral'] - value)
+    if trim_settings['neutral'] > trim_settings['full_up']:  # full up is  lowest value
+        if trim_value <= trim_settings['neutral']:  # positive trim
+            display_percent = round(diff / (trim_settings['neutral'] - trim_settings['full_up']) * 100)
+        else:  # negative trim
+            display_percent = - round(diff / (trim_settings['full_down'] - trim_settings['neutral']) * 100)
+    else:  # full down is lowest value
+        if trim_value >= trim_settings['neutral']:  # positive trim
+            display_percent = round(diff / (trim_settings['full_up'] - trim_settings['neutral']) * 100)
+        else:  # negative trim
+            display_percent = - round(diff / (trim_settings['neutral'] - trim_settings['full_down']) * 100)
+    if display_percent > 100:
+        return 100
+    elif display_percent < -100:
+        return -100
+    return display_percent
+
+
+def calc_rudder_percent(value):
+    diff = abs(trim_settings['rudder_neutral'] - value)
+    if trim_settings['rudder_neutral'] > trim_settings['rudder_left']:  # left is  lowest value
+        if trim_value <= trim_settings['rudder_neutral']:
+            percent = round(diff / (trim_settings['rudder_neutral'] - trim_settings['rudder_left']) * 100)
+        else:  # negative trim
+            percent = - round(diff / (trim_settings['rudder_right'] - trim_settings['rudder_neutral']) * 100)
+    else:  # full down is lowest value
+        if trim_value >= trim_settings['rudder_neutral']:  # positive trim
+            percent = round(diff / (trim_settings['rudder_left'] - trim_settings['rudder_neutral']) * 100)
+        else:  # negative trim
+            percent = - round(diff / (trim_settings['rudder_neutral'] - trim_settings['rudder_right']) * 100)
+    if percent > 100:
+        return 100
+    elif percent < -100:
+        return -100
+    return percent
+
+
 async def display_driver():
-    global trim_value
-    global user_status
     global display_wakeup
     global led_onboard
 
     old_value = 0
+    old_rudder_value = 0
     print('Display driver running.')
-    d = display.Display()
+    d = display.Display(RUDDER_TRIM)
     await uasyncio.sleep_ms(100)  # wait for other coros to finish their measurements
     while True:
         # print('Display driver: user status {:2d}'.format(user_status))
         if user_status == 0 or user_status == 1:
-            diff = abs(trim_settings['neutral'] - trim_value)
-            if trim_settings['neutral'] > trim_settings['full_up']:   # full up is  lowest value
-                if trim_value <= trim_settings['neutral']:    # positive trim
-                    display_percent = round(diff / (trim_settings['neutral'] - trim_settings['full_up']) * 100)
-                else:   # negative trim
-                    display_percent = - round(diff / (trim_settings['full_down'] - trim_settings['neutral']) * 100)
-            else:   # full down is lowest value
-                if trim_value >= trim_settings['neutral']:    # positive trim
-                    display_percent = round(diff / (trim_settings['full_up'] - trim_settings['neutral']) * 100)
-                else:   # negative trim
-                    display_percent = - round(diff / (trim_settings['neutral'] - trim_settings['full_down']) * 100)
-            if display_percent > 100:
-                display_percent = 100
-            elif display_percent < -100:
-                display_percent = -100
-            # print('Percentage {:2d}'.format(display_percent))
-            if display_percent != old_value or display_wakeup > 0:
+            display_percent = calc_display_percent(trim_value)
+            rudder_percent = calc_rudder_percent(rudder_value)
+            if display_percent != old_value or rudder_percent != old_rudder_value or display_wakeup > 0:
                 led_onboard.off()   # do some flicker
                 old_value = display_percent
+                old_rudder_value = rudder_percent
                 display_wakeup -= 1
-                d.indicator(display_percent, main_power, user_status)
+                d.indicator(display_percent, rudder_percent, main_power, user_status)
                 d.print()
                 led_onboard.on()
             else:
                 await uasyncio.sleep_ms(50)
         elif user_status == 2:   # setup trim full up
-            d.indicator(0, 2)
+            d.indicator(100, 0, main_power, user_status)
             d.print()
         elif user_status == 3:   # setup trim neutral
-            d.indicator(0, 3)
+            d.indicator(0, 0, main_power, user_status)
             d.print()
         elif user_status == 4:    # setup trim full down
+            d.indicator(-100, 0, main_power, user_status)
             d.print()
-            d.indicator(0, 4)
+        elif user_status == 5:    # setup rudder trim full left
+            d.indicator(0, 100, main_power, user_status)
+            d.print()
+        elif user_status == 6:    # setup rudder trim neutral
+            d.indicator(0, 0, main_power, user_status)
+            d.print()
+        elif user_status == 7:    # setup rudder trim full right
+            d.indicator(0, -100, main_power, user_status)
+            d.print()
 
         while d.busy():
             await uasyncio.sleep_ms(50)
@@ -128,30 +163,49 @@ def pin_press():
 
 
 def pin_press_short():
+    global new_trim
     global user_status
     global trim_settings
     global trim_value
-    global new_trim
     global display_wakeup
 
-    # print('Short pin pressed')
+    # print('Short pin pressed. User status was {:2d} trim_value {:2d}'.format(user_status, trim_value))
     display_wakeup = 1
-    if user_status == 2:     # highest trim up
-        user_status = 3
+    if user_status == 2:     # start setup sequence, highest trim up
+        new_trim = trim_settings.copy()  # start with old values
         new_trim['full_up'] = trim_value
+        user_status = 3
     elif user_status == 3:   # neutral
-        user_status = 4
         new_trim['neutral'] = trim_value
+        user_status = 4
     elif user_status == 4:    # trim down
-        user_status = 0
         new_trim['full_down'] = trim_value
         # set new trim values, do a sanity check, that neutral is between the two values
-        if new_trim['full_down'] > new_trim['neutral'] > new_trim['full_up']:
-            trim_settings = new_trim
+        if new_trim['full_down'] > new_trim['neutral'] > new_trim['full_up'] or \
+                new_trim['full_down'] < new_trim['neutral'] < new_trim['full_up']:
+            trim_settings = new_trim.copy()
             config.save(trim_settings)
-        elif new_trim['full_down'] < new_trim['neutral'] < new_trim['full_up']:
-            trim_settings = new_trim
+            print('Setting new trim settings: {:s}'.format(json.dumps(trim_settings)))
+        if RUDDER_TRIM:
+            user_status = 5
+        else:
+            user_status = 0
+    elif user_status == 5:     # highest right
+        new_trim['rudder_right'] = rudder_value
+        user_status = 6
+    elif user_status == 6:   # neutral
+        new_trim['rudder_neutral'] = rudder_value
+        user_status = 7
+    elif user_status == 7:    # highest left
+        new_trim['rudder_left'] = rudder_value
+        # set new trim values, do a sanity check, that neutral is between the two values
+        if new_trim['rudder_left'] > new_trim['rudder_neutral'] > new_trim['rudder_right'] or \
+                new_trim['rudder_left'] < new_trim['rudder_neutral'] < new_trim['rudder_right']:
+            trim_settings = new_trim.copy()
             config.save(trim_settings)
+            print('Setting new trim settings: {:s}'.format(json.dumps(trim_settings)))
+        user_status = 0
+    # print('New User status {:2d}'.format(user_status))
 
 
 async def user_interface():
@@ -164,7 +218,7 @@ async def user_interface():
 
     while True:
         await uasyncio.sleep_ms(100)
-        if user_status == 1:   # first long press for setup
+        if user_status == 1:   # first long press for setup was done
             if time.ticks_diff(time.ticks_ms(), start) > SET_TIME_MS:   # you waited too long
                 print('setting user status back to 0')
                 user_status = 0
@@ -172,20 +226,23 @@ async def user_interface():
 
 async def sensor_reader():
     global trim_value
+    global rudder_value
     global main_power
 
     adc_trim = ADC(Pin(26))  # create ADC object on ADC pin
     adc_power = ADC(Pin(27))
+    adc_rudder = ADC(Pin(28))
     print('Sensor reader running.')
     while True:
         v_trim = adc_trim.read_u16() * VOLTAGE_FACTOR    # read value, 0-65535 across voltage range 0.0v - 3.3v
+        v_rudder = adc_rudder.read_u16() * VOLTAGE_FACTOR  # read value, 0-65535 across voltage range 0.0v - 3.3v
         v_power = adc_power.read_u16() * VOLTAGE_FACTOR  # value for power
-        trim_value = round((v_power - v_trim) * 200 / v_power) - 100 # calculates the trim position from -100% to 100%
+        trim_value = round((v_power - v_trim) * 200 / v_power) - 100  # calculates the trim position from -100% to 100%
+        rudder_value = round((v_power - v_rudder) * 200 / v_power) - 100  # calculates the trim position in %
         main_power = v_power * (DIVIDER_R1 + DIVIDER_R2) / DIVIDER_R2
-        print('v_trim {:2f} v_power {:2f} Trim-Value {:2d} main_pwer {:2f}'.format(v_trim, v_power, trim_value,
-                                                                                   main_power))
+        # print('v_trim {:2f.3} v_rudder {:2f.3} v_power {:2f} trim {:2d}% rudder {:2d}% power {:2f.1}'.
+        #       format(v_trim, v_rudder, v_power, trim_value, rudder_value, main_power))
         await uasyncio.sleep_ms(100)
-
 
 
 async def main():
@@ -195,6 +252,9 @@ async def main():
     trim_settings['full_up'] = configuration.get('full_up', trim_default['full_up'])
     trim_settings['neutral'] = configuration.get('neutral', trim_default['neutral'])
     trim_settings['full_down'] = configuration.get('full_down', trim_default['full_down'])
+    trim_settings['rudder_left'] = configuration.get('rudder_left', trim_default['rudder_left'])
+    trim_settings['rudder_neutral'] = configuration.get('rudder_neutral', trim_default['rudder_neutral'])
+    trim_settings['rudder_right'] = configuration.get('rudder_right', trim_default['rudder_right'])
     print('Trim settings: {:s}'.format(json.dumps(trim_settings)))
 
     tasks = [uasyncio.create_task(display_driver()),
